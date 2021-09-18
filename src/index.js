45a7f289cdee0104407af9873efa6e4166b679ca
kumard3 <kumardeepanshu157200@gmail.com>
2021-09-18 23:13:09 +0530

    some changes in dark mode

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import App from './App';
 import './style/index.css'
 
 ReactDOM.render(
-  <div className="dark:bg-gray-800 bg-white " ><App/></div>,
+  <div className="dark:bg-gray-800 bg-white h-full w-full dark:text-white  " ><App/></div>,
   document.getElementById('root')
 );
