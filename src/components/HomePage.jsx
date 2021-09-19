4a62108eca78ab5c10197a1a606ea96e17b3c65a
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2021-09-19 15:03:41 +0530

    Roadmap data initialized

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,12 +1,12 @@
-
-
+import { Tab } from "@headlessui/react";
 import React from "react";
 import { Link } from "react-router-dom";
+import WebDevelopment from "./Roadmaps/WebDevelopment";
 function HomePage() {
   const basic_styles = `flex items-center justify-center`;
   return (
-    <div className={` ${basic_styles} w-full  flex-col h-screen  `}>
-      <h1 className="font-bold text-5xl mt-10"> Student Helper Bot </h1>
+    <div className="">
+      {/* <h1 className="font-bold text-5xl mt-10"> Student Helper Bot </h1>
       <div className={` ${basic_styles} w-full h-full `}>
         <Link to="/chat-bot">
           <div
@@ -22,7 +22,21 @@ function HomePage() {
             <button className="font-bold text-2xl">GO TO COURSE FINDER</button>
           </div>
         </Link>
-      </div>
+      </div> */}
+      <Tab.Group>
+        <Tab.List>
+          <Tab>Web Development</Tab>
+          <Tab>Data Science</Tab>
+          <Tab>Machine Learning</Tab>
+        </Tab.List>
+        <Tab.Panels>
+          <Tab.Panel>
+            <WebDevelopment />
+          </Tab.Panel>
+          <Tab.Panel>Content 2</Tab.Panel>
+          <Tab.Panel>Content 3</Tab.Panel>
+        </Tab.Panels>
+      </Tab.Group>
     </div>
   );
 }
